de2142c988384758dfaa5a68f1f3ccc978b67917
Ebuka Peter <peterdbrainy5@gmail.com>
2020-04-26 23:12:09 +0100

    implemented the image upload feature

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from "../post.model";
+import { mimeType } from "./mime-type.validator";
 
 
 @Component({
@@ -30,7 +31,9 @@ export class PostCreateComponent implements OnInit {
       }),
     'content': new FormControl(null, 
       {validators: [Validators.required]}),
-     'image': new FormControl(null, {validators: [Validators.required]}) 
+     'image': new FormControl(null, {validators: [Validators.required], 
+    asyncValidators: [mimeType]
+    }) 
     });
     this.route.paramMap.subscribe((paramMap: ParamMap)=> {
     if(paramMap.has('postId')){
@@ -70,7 +73,8 @@ export class PostCreateComponent implements OnInit {
     }
     this.isLoading = true;
     if (this.mode === 'create'){
-      this.postsservice.addPost(this.form.value.title, this.form.value.content);
+      this.postsservice.addPost(
+        this.form.value.title, this.form.value.content, this.form.value.image);
     }else{
       this.postsservice.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
