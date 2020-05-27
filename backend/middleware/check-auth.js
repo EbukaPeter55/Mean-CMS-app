d2f0d071701204a909b8998a1b093255f83eaf7f
Ebuka Peter <peterdbrainy5@gmail.com>
2020-05-27 16:48:15 +0100

    added a nodemon file for backend global configuration

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,11 +4,11 @@ module.exports = (req, res, next) => {
 // Check the headers to see if a token is present and render appropriate messages
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "secret_this_should_be_longer");
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = {email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch (error) {
-    res.status(401).json({ message: "You are not authenticated!" });
+    res.status(401).json({ message: "You are not authenticated!"});
   }
 };
 
